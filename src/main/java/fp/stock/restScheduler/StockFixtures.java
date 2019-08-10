ee5276ce5632026af538a8320cd2d5bff82e3226
Piotr <piotrekchorzow@interia.eu>
2019-08-10 21:39:58 +0200

    tezting REST client on random example

--- src/main/java/fp/stock/restScheduler/StockFixtures.java
+++ src/main/java/fp/stock/restScheduler/StockFixtures.java
@@ -1,4 +1,17 @@
 package fp.stock.restScheduler;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StockFixtures {
+
+
+    private LocalDateTime publicationDate;
+    List<StockItem> items;
 }
--- src/main/java/fp/stock/restScheduler/StockItem.java
+++ src/main/java/fp/stock/restScheduler/StockItem.java
@@ -1,4 +1,18 @@
 package fp.stock.restScheduler;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StockItem {
+
+    private String name;
+    private String code;
+    private String unit;
+    private String price;
+
 }
